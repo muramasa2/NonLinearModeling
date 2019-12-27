90f939d49f5db35d2e66498fb957b8e352ace828
muramasa2 <4318571@ed.tus.ac.jp>
2019-12-27 22:03:29 +0900

    音楽データの読み込みを可能に train_music.py, test_music.py追加

---
+++ code/test_music.py
@@ -0,0 +1,266 @@
+"""test denoise or modeling nonlinear noise."""
+##################
+# import library #
+##################
+import os
+import argparse
+import numpy as np
+from glob import glob
+import soundfile as sf
+from datetime import date
+from scipy import hanning
+from scipy.fftpack import fft
+from natsort import natsorted
+import matplotlib.pyplot as plt
+from keras.layers import CuDNNLSTM
+from keras.models import Sequential
+from keras.losses import mean_squared_error
+from keras.layers.pooling import MaxPooling1D
+from keras.layers.convolutional import Conv1D, UpSampling1D
+
+
+####################
+# load signal data #
+####################
+parser = argparse.ArgumentParser()
+parser.add_argument('--structure', '-s', type=str, default='LSTM')
+parser.add_argument('--reg', '-r', type=str, default='off')
+
+parser.add_argument('--input_length', '-I', type=int, default=1024)
+parser.add_argument('--output_length', '-O', type=int, default=1024)
+parser.add_argument('--step', '-S', type=int, default=64)
+
+args = parser.parse_args()
+
+structure = args.structure  # Conv1D or LSTM
+reg = args.reg  # on or off
+
+in_len = args.input_length
+out_len = args.output_length
+step = args.step
+
+print('structure:', structure)
+print('reg:', reg)
+
+print('------------------------')
+print('input_length:', in_len)
+print('output_length:', out_len)
+print('step:', step)
+
+input_path = '../data/wav/beat_hirei_nuforce_curve_1.wav'
+output_path = '../data/wav/05 Beat It.wav'
+label = ['pred_denoise', 'true_clean', 'distorted']
+
+input_data = []
+output_data = []
+
+##################
+# make test data #
+##################
+in_signal, fs = sf.read(input_path)
+out_signal, _ = sf.read(output_path)
+
+if reg == 'on':
+    in_max = max(abs(in_signal))
+    out_max = max(abs(out_signal))
+    in_signal = in_signal/in_max
+    out_signal = out_signal/out_max
+
+for n in range(int((len(in_signal)-(in_len))/step)):
+    input_data.append(in_signal[int(n * step):int(n * step + in_len)])
+    output_data.append(out_signal[int(n * step):int(n * step + out_len)])
+
+input_data = np.array(input_data)
+output_data = np.array(output_data)
+
+test_input_data = input_data[int(len(input_data)*0.8):]
+test_output_data = output_data[int(len(output_data)*0.8):]
+
+testX = test_input_data.reshape(-1, in_len, 1)
+testy = test_output_data.reshape(-1, out_len, 1)
+
+print('testX shape:', testX.shape)
+print('testy shape:', testy.shape)
+
+###############
+# build model #
+###############
+
+
+class LossFunc:
+    """Loss mse."""
+
+    def __init__(self, timesteps):
+        """Init."""
+        self.__name__ = "LossFunc"
+        self.timesteps = timesteps
+
+    def __call__(self, y_true, y_pred):
+        """Call."""
+        return mean_squared_error(
+            y_true[:, -self.timesteps:, :],
+            y_pred[:, -self.timesteps:, :])
+
+
+model = Sequential()
+
+dsif structure == 'Conv1D':
+    model.add(Conv1D(64, 8, padding='same',
+                     input_shape=(in_len, 1), activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(32, 8, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+
+    model.add(Conv1D(32, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(1, 8, padding='same', activation='tanh'))
+    model.compile(optimizer='adam', loss='mse')
+
+elif structure == 'LSTM':
+    model.add(CuDNNLSTM(64, input_shape=(in_len, 1), return_sequences=True))
+    model.add(CuDNNLSTM(64, return_sequences=True))
+    model.add(CuDNNLSTM(1, return_sequences=True))
+
+    if in_len == out_len:
+        model.compile(optimizer='adam', loss='mse')
+    else:
+        model.compile(optimizer='adam', loss=LossFunc(out_len))
+
+model.summary()
+
+year = date.today().year
+month = date.today().month
+day = date.today().day
+model_save_path = f'../weight/{year}{month}{day}/{structure}_{in_len}_{out_len}_{step}.h5'
+model.load_weights(model_save_path)
+
+
+########
+# pred #
+########
+for i in range(len(testX)):
+    predy = model.predict(testX[i].reshape(1, in_len, 1))
+
+    if i == 0:
+        predict = predy
+    else:
+        predict = np.concatenate((predict, predy[:, in_len-step:]), axis=1)
+
+input = in_signal[:len(predict[0])]
+output = out_signal[:len(predict[0])]
+
+if reg == 'on':
+    predict = predict*out_max
+    input = input*in_max
+    output = output*out_max
+
+plt.rcParams["font.size"] = 15  # 全体のフォントサイズが変更されます。
+plt.rcParams['xtick.direction'] = 'in'  # x axis in
+plt.rcParams['ytick.direction'] = 'in'  # y axis in
+plt.rcParams['axes.linewidth'] = 1.0  # axis line width
+plt.rcParams['axes.grid'] = True  # make grid
+plt.rcParams['figure.dpi'] = 300
+
+t = np.arange(0, (len(input))/fs, 1 / fs)
+plt.figure()
+plt.plot(t, input, 'r', linewidth=3, label=label[2])
+plt.plot(t, output, 'g', linewidth=3, label=label[1])
+plt.plot(t, predict[0], 'b', linewidth=3, label=label[0])
+plt.xlabel('time[s]')
+plt.ylabel('Amplitude[V]')
+plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+plt.savefig(f'../figure/{year}{month}{day}/signal_{structure}_{in_len}_{out_len}_{step}.jpg',
+            bbox_inches="tight", pad_inches=0.05)
+
+os.makedirs(f'../result_wave/{year}{month}{day}', exist_ok=True)
+sf.write(f'../result_wave/{year}{month}{day}/{wav_num}_{structure}_{in_len}_{out_len}_{step}.wav',
+         predict[0], 44100, subtype='PCM_16')  # 16bit 44.1kHz
+
+
+def signal_fft(signal, N):  # FFTするsignal長と窓長Nは同じサンプル数に固定する
+    """Fft siganl."""
+    win = hanning(N)  # 窓関数
+    spectrum = fft(signal*win)  # フーリエ変換
+    spectrum_abs = np.abs(spectrum)  # 振幅を元に信号に揃える
+    half_spectrum = spectrum_abs[:int(N/2)]
+    half_spectrum[0] = half_spectrum[0] / 2  # 直流成分（今回は扱わないけど）は2倍不要
+    half_spectrum_dBV = 20*np.log10(half_spectrum)
+
+    return spectrum, half_spectrum_dBV
+
+
+path = f'./result_wave/{year}{month}{day}/{wav_num}_{structure}_{in_len}_{out_len}_{step}.wav'
+out_data, fs = sf.read(path)
+_, out_half_spectrum_dBV = signal_fft(out_data, len(out_data))
+f2 = np.arange(0, fs/2, (fs/2)/out_half_spectrum_dBV.shape[0])  # 横軸周波数軸[Hz]
+plt.semilogx(f2, out_half_spectrum_dBV)
+
+
+in_data, fs = sf.read(output_paths[wav_num])
+in_data = in_data[:len(out_data)]
+_, in_half_spectrum_dBV = signal_fft(in_data, len(in_data))
+f1 = np.arange(0, fs/2, (fs/2)/in_half_spectrum_dBV.shape[0])  # 横軸周波数軸[Hz]
+plt.semilogx(f1, in_half_spectrum_dBV)
+
+
+plt.rcParams["font.size"] = 15  # 全体のフォントサイズが変更されます。
+plt.rcParams['xtick.direction'] = 'in'  # x axis in
+plt.rcParams['ytick.direction'] = 'in'  # y axis in
+plt.rcParams['axes.linewidth'] = 1.0  # axis line width
+plt.rcParams['axes.grid'] = True  # make grid
+plt.rcParams['figure.dpi'] = 300
+
+
+# plot
+plt.figure(figsize=(15, 10))  # figure size in inch, 横×縦
+plt.semilogx(f1, out_half_spectrum_dBV, 'r', label=label[0])
+plt.semilogx(f1, in_half_spectrum_dBV, 'b', label=label[1])
+plt.xlim([1, 22050])
+plt.xlabel('Frequency[Hz]', fontsize=15)
+plt.ylabel('Amplitude[dB]', fontsize=15)
+plt.legend(loc='upper right', fontsize=15)
+
+# save
+plt.savefig(f'figure/{year}{month}{day}/fft_{wav_num}_{wav_num}_{structure}_{in_len}_{out_len}_{step}.jpg',
+            bbox_inches="tight", pad_inches=0.05)
+
+sub = max(in_half_spectrum_dBV)-max(out_half_spectrum_dBV)
+print('subtract:', sub)
+in_half_spectrum_dBV = in_half_spectrum_dBV - sub
+
+# plot
+plt.figure(figsize=(15, 10))  # figure size in inch, 横×縦
+plt.semilogx(f1, out_half_spectrum_dBV, 'r', label=label[0])
+plt.semilogx(f1, in_half_spectrum_dBV, 'b', label=label[1])
+plt.xlim([1, 22050])
+plt.xlabel('Frequency[Hz]', fontsize=15)
+plt.ylabel('Amplitude[dB]', fontsize=15)
+plt.legend(loc='upper right', fontsize=15)
+
+# save
+plt.savefig(f'figure/{year}{month}{day}/fft_{wav_num}_{wav_num}_{structure}_{in_len}_{out_len}_{step}.jpg',
+            bbox_inches="tight", pad_inches=0.05)
+
+
+# def THD(spectrum, n, f):
+#     V = 10**(spectrum[f]/20)
+#     lin_V = []
+#
+#     for i in range(2,n+1):
+#         lin_V.append(10**(spectrum[f*i]/20))
+#
+#     lin_V = np.array(lin_V)**2
+#     thd = sum(lin_V)/V
+#
+#     return thd
+#
+# f = 1019
+# in_thd = THD(in_half_spectrum_dBV, 10, f)
+# out_thd = THD(out_half_spectrum_dBV, 10, f)
+# print(20*np.log10(in_thd), 20*np.log10(out_thd))
---
+++ code/train_music.py
@@ -0,0 +1,213 @@
+"""train denoise or modeling nonlinear noise by using generator."""
+##################
+# import library #
+##################
+import os
+import argparse
+import numpy as np
+import soundfile as sf
+from datetime import date
+import matplotlib.pyplot as plt
+from keras.layers import CuDNNLSTM
+from keras.models import Sequential
+from keras.losses import mean_squared_error
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers.pooling import MaxPooling1D
+from keras.layers.convolutional import Conv1D, UpSampling1D
+
+
+####################
+# load signal data #
+####################
+parser = argparse.ArgumentParser()
+parser.add_argument('--structure', '-s', type=str, default='LSTM')
+parser.add_argument('--reg', '-r', type=str, default='off')
+
+parser.add_argument('--batch_size', '-B', type=int, default=32)
+parser.add_argument('--epochs', '-E', type=int, default=100)
+
+parser.add_argument('--input_length', '-I', type=int, default=1024)
+parser.add_argument('--output_length', '-O', type=int, default=1024)
+parser.add_argument('--step', '-S', type=int, default=64)
+
+
+args = parser.parse_args()
+
+structure = args.structure  # Conv1D or LSTM
+reg = args.reg  # on or off
+
+batch_size = args.batch_size
+epochs = args.epochs
+in_len = args.input_length
+out_len = args.output_length
+step = args.step
+
+
+print('structure:', structure)
+print('reg:', reg)
+
+print('------------------------')
+print('batch_size:', batch_size)
+print('epochs:', epochs)
+print('input_length:', in_len)
+print('output_length:', out_len)
+print('step:', step)
+
+input_path = '../data/wav/beat_hirei_nuforce_curve_1.wav'
+output_path = '../data/wav/05 Beat It.wav'
+
+########################
+# make train, val data #
+########################
+input_data = []
+output_data = []
+
+in_signal, fs = sf.read(input_path)
+
+out_signal, _ = sf.read(output_path)
+R_out_signal = out_signal[:,0]
+out_signal = R_out_signal[:len(in_signal)]
+
+if reg == 'on':
+    in_max = max(abs(in_signal))
+    out_max = max(abs(out_signal))
+    in_signal = in_signal/in_max
+    out_signal = out_signal/out_max
+
+for n in range(int((len(in_signal)-(in_len))/step)):
+    input_data.append(in_signal[int(n * step):int(n * step + in_len)])
+    output_data.append(out_signal[int(n * step):int(n * step + out_len)])
+
+input_data = np.array(input_data)
+output_data = np.array(output_data)
+
+np.random.seed(0)
+np.random.shuffle(input_data)
+np.random.seed(0)
+np.random.shuffle(output_data)
+
+train_input_data = input_data[:int(len(input_data)*0.6)]
+train_output_data = output_data[:int(len(output_data)*0.6)]
+val_input_data = input_data[int(len(input_data)*0.6):int(len(input_data)*0.8)]
+val_output_data = output_data[int(len(output_data)*0.6):int(len(output_data)*0.8)]
+
+trainX = train_input_data.reshape(-1, in_len, 1)
+trainy = train_output_data.reshape(-1, out_len, 1)
+valX = val_input_data.reshape(-1, in_len, 1)
+valy = val_output_data.reshape(-1, out_len, 1)
+
+np.random.seed(0)
+np.random.shuffle(trainX)
+np.random.seed(0)
+np.random.shuffle(trainy)
+
+np.random.seed(0)
+np.random.shuffle(valX)
+np.random.seed(0)
+np.random.shuffle(valy)
+
+print('trainX shape:', trainX.shape)
+print('trainy shape:', trainy.shape)
+print('valX shape:', valX.shape)
+print('valy shape:', valy.shape)
+
+###############
+# build model #
+###############
+year = date.today().year
+month = date.today().month
+day = date.today().day
+os.makedirs(f'../weight/{year}{month}{day}', exist_ok=True)
+os.makedirs(f'../figure/{year}{month}{day}', exist_ok=True)
+model_save_path = f'../weight/{year}{month}{day}/{structure}_{in_len}_{out_len}_{step}.h5'
+
+cp_cb = ModelCheckpoint(filepath=model_save_path, monitor='val_loss',
+                        verbose=1, save_weights_only=True,
+                        save_best_only=True, mode='auto')
+es_cb = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='auto')
+
+model = Sequential()
+
+
+class LossFunc:
+    """Loss mse."""
+
+    def __init__(self, timesteps):
+        """Init."""
+        self.__name__ = "LossFunc"
+        self.timesteps = timesteps
+
+    def __call__(self, y_true, y_pred):
+        """Call."""
+        return mean_squared_error(
+            y_true[:, -self.timesteps:, :],
+            y_pred[:, -self.timesteps:, :])
+
+
+if structure == 'Conv1D':
+    model.add(Conv1D(64, 8, padding='same',
+                     input_shape=(in_len, 1), activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(32, 8, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+
+    model.add(Conv1D(32, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(64, 8, padding='same', activation='relu'))
+    model.add(UpSampling1D(2))
+    model.add(Conv1D(1, 8, padding='same', activation='tanh'))
+    model.compile(optimizer='adam', loss='mse')
+
+elif structure == 'LSTM':
+    model.add(CuDNNLSTM(64, input_shape=(in_len, 1), return_sequences=True))
+    model.add(CuDNNLSTM(64, return_sequences=True))
+    model.add(CuDNNLSTM(1, return_sequences=True))
+
+    if in_len == out_len:
+        model.compile(optimizer='adam', loss='mse')
+    else:
+        model.compile(optimizer='adam', loss=LossFunc(out_len))
+
+model.summary()
+
+
+def generator(input, output, batch):
+    """Generate data."""
+    print('step:', (len(input) // batch_size))
+
+    while True:
+        for num_batch in range(1, len(input) // batch+1):  # mini-batch loop
+            X = input[num_batch*batch_size:(num_batch*batch+batch), :, :]
+            y = output[num_batch*batch_size:(num_batch*batch+batch), :, :]
+            yield (X, y)
+            print('\nnum_batch:', num_batch, '\n')
+
+
+history = model.fit_generator(generator(trainX, trainy, batch_size),
+                              validation_data=(valX, valy), epochs=epochs,
+                              steps_per_epoch=len(trainX) // batch_size,
+                              max_queue_size=int(len(trainX) // batch_size),
+                              verbose=1, callbacks=[cp_cb, es_cb])
+
+print('Finish learning!')
+
+plt.figure(1)
+epoch = np.arange(len(history.history['loss']))
+plt.plot(epoch, history.history['loss'], label='loss')
+plt.plot(epoch, history.history['val_loss'], label='val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig(f'../figure/{year}{month}{day}/{structure}_{in_len}_{out_len}_{step}.jpg')
+
+plt.figure(2)
+plt.plot(epoch, history.history['acc'], label='acc')
+plt.plot(epoch, history.history['val_acc'], label='val_acc')
+plt.xlabel('Epoch')
+plt.ylabel('MSE')
+plt.legend()
+plt.savefig(f'../figure/{year}{month}{day}/{structure}_{in_len}_{out_len}_{step}.jpg')
